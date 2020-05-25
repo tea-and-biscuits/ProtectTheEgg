0ead5493af70197595359f56c27f31c2769626c9
Harieo <harieo@hotmail.com>
2020-05-25 12:29:03 +0100

    Initial commit

---
+++ src/main/java/uk/co/harieo/quackbedwars/ProtectTheEgg.java
@@ -0,0 +1,22 @@
+package uk.co.harieo.quackbedwars;
+
+import uk.co.harieo.minigames.games.Minigame;
+
+public class ProtectTheEgg extends Minigame {
+
+	@Override
+	public String getMinigameName() {
+		return "Protect the Egg";
+	}
+
+	@Override
+	public int getMaxPlayers() {
+		return 0;
+	}
+
+	@Override
+	public int getOptimalPlayers() {
+		return 0;
+	}
+
+}
